39d0ec2352110380235f5d383e98a6091cdde532
reijiotake <reiji.otake@zdh.co.jp>
2025-02-10 16:50:25 +0900

    Update 01_ADLS_to_Fabric_Lakehouse_by_notebook.py

--- fabric/official_dataset(100TB)/01_ADLS_to_Fabric_Lakehouse_by_notebook.py
+++ fabric/official_dataset(100TB)/01_ADLS_to_Fabric_Lakehouse_by_notebook.py
@@ -18,5 +18,8 @@ spark.conf.set("spark.sql.autoCompact.maxFileSize", "536870912")
 df = spark.read.parquet("Files/<file_name>/<Table_name>/*.parquet")
 df.write.mode("overwrite").format("delta").save("Tables/Table_name")
 
-df = spark.read.parquet("Files/tpcds-sf100tcl/<Table_name>/*.parquet") 
+df = spark.read.parquet("Files/tpcds-sf100tcl/<Table_name>/part-00*.parquet") 
+df.write.mode("append").format("delta").save("Tables/store_sales")
+
+df = spark.read.parquet("Files/tpcds-sf100tcl/<Table_name>/part-01~5*.parquet") 
 df.write.mode("append").format("delta").save("Tables/store_sales")
