b73d3a6a54ddb84b89c79807bcf158ad302a0fc8
reijiotake <reiji.otake@zdh.co.jp>
2025-02-10 16:16:46 +0900

    Update 01_ADLS to Fabric Lakehouse by Notebook.py

--- fabric/Snowflake_dataset(100TB)/01_ADLS to Fabric Lakehouse by Notebook.py
+++ fabric/Snowflake_dataset(100TB)/01_ADLS to Fabric Lakehouse by Notebook.py
@@ -1,6 +1,7 @@
 # 新規レイクハウスを作成、ショートカットでTCP-DSデータが格納されているADLSにショートカットを作成する
 # 新規ノートブック作成後、レイクハウスをアタッチする。
 
+## 一括取り込みの場合
 spark.conf.set("spark.sql.delta.optimizeWrite.enabled", "true")
 spark.conf.set("spark.sql.delta.autoCompact.enabled", "true")
 spark.conf.set("spark.sql.autoCompact.maxFileSize", "536870912")
@@ -8,3 +9,14 @@ spark.conf.set("spark.sql.autoCompact.maxFileSize", "536870912")
 
 df = spark.read.parquet("Files/tpcds-sf100tcl/<Table_name>/*.parquet")
 df.write.mode("overwrite").format("delta").save("Tables/Table_name")
+
+## 分割取り込みの場合
+spark.conf.set("spark.sql.delta.optimizeWrite.enabled", "true")
+spark.conf.set("spark.sql.delta.autoCompact.enabled", "true")
+spark.conf.set("spark.sql.autoCompact.maxFileSize", "536870912")
+
+df = spark.read.parquet("Files/tpcds-sf100tcl/<Table_name>/1/*.parquet")
+df.write.mode("overwrite").format("delta").save("Tables/Table_name")
+
+df = spark.read.parquet("Files/tpcds-sf100tcl/<Table_name>/2/*.parquet") 
+df.write.mode("append").format("delta").save("Tables/store_sales")
